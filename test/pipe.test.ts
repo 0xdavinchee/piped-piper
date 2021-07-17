6171b1b0954e96a06f77571efb3eba3493b0ffba
Vincent Chee <vincent.kh.chee@gmail.com>
2021-07-17 21:05:02 +0800

    basic withdrawal tests implemented

--- test/pipe.test.ts
+++ test/pipe.test.ts
@@ -284,11 +284,11 @@ describe("SuperValve Tests", () => {
      * Helper to execute Create/Update/Delete Flow to SuperValve test.
      * Has expect checks to ensure flow rate is as expected and also has console.log's
      * indicating what it is testing.
-     * @param func 
-     * @param userAddress 
-     * @param flowRate 
-     * @param userData 
-     * @param incrementer 
+     * @param func
+     * @param userAddress
+     * @param flowRate
+     * @param userData
+     * @param incrementer
      */
     const executeModifyFlowTest = async (
         func: any,
@@ -312,7 +312,7 @@ describe("SuperValve Tests", () => {
 
     /**
      * Runs executeModifyFlowTest for all users.
-     * @param func 
+     * @param func
      * @param flowRate if this is null, we randomize this
      * @param userData if this is null, we randomize this
      * @param incrementer if this is null, it is 0
@@ -327,7 +327,7 @@ describe("SuperValve Tests", () => {
             await executeModifyFlowTest(func, userAddresses[i], flowRate, userData, incrementer);
         }
     };
-    
+
     const executeExpectRevertModifyFlowTest = async (
         func: any,
         sender: string,
@@ -335,18 +335,9 @@ describe("SuperValve Tests", () => {
         revertString: string,
         flowRate?: number,
     ) => {
-        let revertedModifyFlowPromise = expectRevertedModifyFlow(
-            func,
-            sender,
-            superValveAddress,
-            userData,
-            flowRate,
-        );
+        let revertedModifyFlowPromise = expectRevertedModifyFlow(func, sender, superValveAddress, userData, flowRate);
 
-        await checkRevertResults(
-            revertedModifyFlowPromise,
-            revertString,
-        );
+        await checkRevertResults(revertedModifyFlowPromise, revertString);
     };
 
     describe("Admin Permissions Tests", () => {
@@ -600,32 +591,18 @@ describe("SuperValve Tests", () => {
                 sf.cfa.deleteFlow,
                 userAddresses[0],
                 deleteFlowData,
-                "CFA: flow does not exist"
+                "CFA: flow does not exist",
             );
         });
     });
 
-    describe.only("Withdrawal Tests", () => {
+    describe("Withdrawal/Deposit Tests", () => {
         it("Should be able to withdraw funds while flows are ongoing.", async () => {
-            let userData;
-            let results;
             const PIPE_ADDRESSES = [Admin.VaultPipe.address, Admin.VaultPipe2.address];
+            let flowAfterThirtyDays = [];
 
-            for (let i = 0; i < userAddresses.length; i++) {
-                const randomFlowRate = Math.floor(Math.random() * 1000);
-                userData = getRandomAllocationsUserData();
-
-                // create another flow
-                results = await modifyFlow(
-                    sf.cfa.createFlow,
-                    Admin.SuperValve,
-                    userAddresses[i],
-                    superValveAddress,
-                    userData,
-                    randomFlowRate,
-                );
-                checkUserFlowRateResults(randomFlowRate, results, userData, userAddresses[i]);
-            }
+            // create flow to superValve
+            await executeModifyFlowTestsForUsers(sf.cfa.createFlow, null);
 
             // go 30 days into the future
             await hre.network.provider.send("evm_increaseTime", [86400 * 30]);
@@ -633,8 +610,8 @@ describe("SuperValve Tests", () => {
 
             // get user total flowed
             for (let i = 0; i < userAddresses.length; i++) {
-                const userTotalFlowed = await Admin.SuperValve.getUserTotalFlowedBalance(userAddresses[i]);
-                console.log(names[userAddresses[i]] + " flow balance after 30 days: " + userTotalFlowed);
+                const [userFlowBalance] = await Admin.SuperValve.getUserTotalFlowedBalance(userAddresses[i]);
+                flowAfterThirtyDays.push(userFlowBalance);
             }
 
             // withdraw funds from super valve
@@ -645,9 +622,76 @@ describe("SuperValve Tests", () => {
 
             // get user total flowed after withdrawal
             for (let i = 0; i < userAddresses.length; i++) {
-                const userTotalFlowed = await Admin.SuperValve.getUserTotalFlowedBalance(userAddresses[i]);
-                console.log(names[userAddresses[i]] + " flow balance after withdrawal: " + userTotalFlowed);
+                const [userFlowBalance] = await Admin.SuperValve.getUserTotalFlowedBalance(userAddresses[i]);
+                console.log(
+                    "Expect " +
+                        names[userAddresses[i]] +
+                        " flow balance after withdrawal: " +
+                        userFlowBalance +
+                        " to be less than flow balance after 30 days: " +
+                        flowAfterThirtyDays[i],
+                );
+                expect(toNum(userFlowBalance)).to.be.lessThan(toNum(flowAfterThirtyDays[i]));
             }
+        });
+
+        it("Should be able to withdraw funds after stopping flows.", async () => {
+            const PIPE_ADDRESSES = [Admin.VaultPipe.address, Admin.VaultPipe2.address];
+            let flowAfterThirtyDays = [];
+            const deleteFlowData = getModifyFlowUserData([
+                { pipeAddress: Admin.VaultPipe.address, percentage: "0" },
+                { pipeAddress: Admin.VaultPipe2.address, percentage: "0" },
+            ]);
+
+            // create flow to superValve
+            await executeModifyFlowTestsForUsers(sf.cfa.createFlow, null);
+
+            // go 30 days into the future
+            await hre.network.provider.send("evm_increaseTime", [86400 * 30]);
+            await hre.network.provider.send("evm_mine");
+
+            // delete flows
+            await executeModifyFlowTestsForUsers(sf.cfa.deleteFlow, 0, deleteFlowData);
+
+            // get user total flowed
+            for (let i = 0; i < userAddresses.length; i++) {
+                const [userFlowBalance] = await Admin.SuperValve.getUserTotalFlowedBalance(userAddresses[i]);
+                flowAfterThirtyDays.push(userFlowBalance);
+            }
+
+            // withdraw funds from super valve
+            for (let i = 0; i < Users.length; i++) {
+                console.log(names[Users[i].address] + " withdrawing from SuperValve.");
+                await Users[i].SuperValve.withdraw(PIPE_ADDRESSES);
+            }
+
+            // get user total flowed after withdrawal
+            for (let i = 0; i < userAddresses.length; i++) {
+                const [userFlowBalance] = await Admin.SuperValve.getUserTotalFlowedBalance(userAddresses[i]);
+                console.log(
+                    "Expect " +
+                        names[userAddresses[i]] +
+                        " flow balance after withdrawal: " +
+                        userFlowBalance +
+                        " to be 0 after stopping flows and withdrawals",
+                );
+                expect(toNum(userFlowBalance)).to.be.eq(0);
+            }
+        });
+
+        it("Should not be able to withdraw from invalid pipe address.", async () => {
+            const INVALID_PIPE_ADDRESSES = [Alice.address, Bob.address];
+
+            // create flow to superValve
+            await executeModifyFlowTest(sf.cfa.createFlow, userAddresses[0], null);
+
+            // go 30 days into the future
+            await hre.network.provider.send("evm_increaseTime", [86400 * 30]);
+            await hre.network.provider.send("evm_mine");
+
+            await expect(Admin.SuperValve.withdraw(INVALID_PIPE_ADDRESSES)).to.be.revertedWith(
+                "SuperValve: This is not a registered vault address.",
+            );
         });
     });
 });
